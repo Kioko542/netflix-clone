91c08e402fae5dff0b41bdbdfaf1b5e6a11e2a0b
Emmanue <kioxmanu7@gmail.com>
2023-11-19 21:15:25 +0300

    firbase config

---
+++ my-app/src/Firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDs_XWsG9hPuZFYJat5eo7F4L1O-7CIFns",
+  authDomain: "netflixcloneproject-50b2e.firebaseapp.com",
+  projectId: "netflixcloneproject-50b2e",
+  storageBucket: "netflixcloneproject-50b2e.appspot.com",
+  messagingSenderId: "980558193985",
+  appId: "1:980558193985:web:866887ada5e61279842dc0"
+};
+
+const app = initializeApp(firebaseConfig);
