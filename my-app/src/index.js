873c3bfac2d3c8501057a06d1a01ace3dc040eb4
Emmanue <kioxmanu7@gmail.com>
2023-11-25 17:27:28 +0300

    Add basename to BrowserRouter in index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='netflix-clone'>
+    <BrowserRouter basename='/netflix-clone'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
